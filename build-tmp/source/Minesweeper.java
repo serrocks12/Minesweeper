a5779d8ba43a60ee5529b02c6693026f8f64370c
unknown <sebenchohra@697-334-Erech.sf.k12.internal>
2015-03-09 12:20:04 -0700

    Add New Changes mines

--- build-tmp/source/Minesweeper.java
+++ build-tmp/source/Minesweeper.java
@@ -1,11 +1,29 @@
-import de.bezier.guido.*;
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import de.bezier.guido.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Minesweeper extends PApplet {
+
+
 public static int NUM_ROWS = 20;
 public static int NUM_COLS = 20;
 private MSButton[][] buttons;
 private ArrayList <MSButton> bombs = new ArrayList <MSButton>();
 private boolean gameOver = false;
 
-void setup ()
+public void setup ()
 {
     size(400, 400);
     textAlign(CENTER,CENTER);
@@ -187,4 +205,13 @@ public class MSButton
             numBombs++;
         return numBombs;
     }
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Minesweeper" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
